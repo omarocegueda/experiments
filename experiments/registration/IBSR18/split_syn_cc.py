ed442b89bd55648148b3f4bc375f17392f077253
omarocegueda <jomaroceguedag@gmail.com>
2014-11-03 17:31:54 -0600

    RF: renamed job file for syn_cc

--- experiments/registration/IBSR18/split_syn_cc.py
+++ experiments/registration/IBSR18/split_syn_cc.py
@@ -2,5 +2,5 @@ import sys
 import experiments.registration.IBSR18.split_tools as st
 if __name__=="__main__":
     required_files = ['../dipyreg.py',
-                      'jobFullSyNCC.sh']
+                      'job_syn_cc.sh']
     st.split_script(sys.argv, required_files)
