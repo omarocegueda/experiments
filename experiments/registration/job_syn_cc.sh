aeb573dbe862bd62bc6418c182f1e3eeb2f18b01
omarocegueda <jomaroceguedag@gmail.com>
2014-11-23 19:24:01 -0600

    set execution flag to shell scripts. Change default iterations

--- experiments/registration/job_syn_cc.sh
+++ experiments/registration/job_syn_cc.sh
@@ -39,5 +39,5 @@ else
     echo "Affine mapping found ($affine). Skipping affine registration."
 fi
 #Diffeomorphic registration
-python dipyreg.py target/$target reference/$reference $affine warp --metric=CC[1.7,4] --iter=10,10,5 --step_length=0.25
+python dipyreg.py target/$target reference/$reference $affine warp --metric=CC[1.7,4] --iter=100,100,25 --step_length=0.25
 date
