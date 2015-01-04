24b012b26f9d719213d6d48f911b4f66e5bf7e13
Omar Ocegueda <jomaroceguedag@gmail.com>
2015-01-04 15:18:51 -0600

    NF: use isotropic scale space for affine registration

--- experiments/registration/job_affine_mi.sh
+++ experiments/registration/job_affine_mi.sh
@@ -28,5 +28,5 @@ targetbase="${targetbase%.*}"
 referencebase="${reference%.*}"
 referencebase="${referencebase%.*}"
 #Affine registration with dipy
-python dipyreg_affine.py target/$target reference/$reference warp --transforms=TRANSLATION,RIGID,AFFINE --metric=MI[32] --iter=10000,10000,10000 --ss_sigma_factor=1.0
+python dipyreg_affine.py target/$target reference/$reference warp --transforms=TRANSLATION,RIGID,AFFINE --metric=MI[32] --iter=1000,1000,1000 --factors=4,2,1 --sigmas=3,1,0 --method=CGGS
 date
