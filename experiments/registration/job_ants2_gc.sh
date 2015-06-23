b273dd9c45f846274ef43792b1cdb6c4fd06c50b
omarocegueda <jomaroceguedag@gmail.com>
2015-06-22 20:55:39 -0500

    Increase memory requirement for ANTS2 with gc. Add execution flag to shell script.

--- experiments/registration/job_ants2_gc.sh
+++ experiments/registration/job_ants2_gc.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 ####################################################
 #Author: Omar Ocegueda (omar@cimat.mx)
-#PBS -l mem=8GB
-#PBS -l pmem=8GB
-#PBS -l vmem=8GB
+#PBS -l mem=16GB
+#PBS -l pmem=16GB
+#PBS -l vmem=16GB
 #PBS -l nodes=1:ppn=1
 #PBS -l walltime=06:00:00
 #PBS -N ANTS2-GC
