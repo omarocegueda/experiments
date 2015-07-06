1df4f6f394f5b375d56b0847116a300cbe0f2acb
Omar Ocegueda <jomaroceguedag@gmail.com>
2015-07-06 09:56:16 -0500

    Add dwi correction script for ants2

---
+++ experiments/registration/split_dwi_ants2_mi.py
@@ -0,0 +1,7 @@
+import sys
+import os
+import experiments.registration.split_tools as st
+if __name__=="__main__":
+    req_dir = os.path.dirname(os.path.realpath(__file__))
+    required_files = [req_dir+'/job_ants2_affine_mi.sh']
+    st.split_dwi(sys.argv, required_files)
