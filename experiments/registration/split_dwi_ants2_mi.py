7196128871d8d27812fc6e92a31059dd39cd8b6b
Omar Ocegueda <jomaroceguedag@gmail.com>
2015-07-06 10:47:48 -0500

    use rigid in split script

--- experiments/registration/split_dwi_ants2_mi.py
+++ experiments/registration/split_dwi_ants2_mi.py
@@ -3,5 +3,5 @@ import os
 import experiments.registration.split_tools as st
 if __name__=="__main__":
     req_dir = os.path.dirname(os.path.realpath(__file__))
-    required_files = [req_dir+'/job_ants2_affine_mi.sh']
+    required_files = [req_dir+'/job_ants2_rigid_mi.sh']
     st.split_dwi(sys.argv, required_files)
