e5177c992e1b630fc7270aa8aaef88ad8746423c
Omar Ocegueda <jomaroceguedag@gmail.com>
2015-09-12 21:41:01 -0500

    NF: add cluster job for sphere2cup

---
+++ experiments/registration/job_sphere2cup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+####################################################
+#Author: Omar Ocegueda (omar@cimat.mx)
+#PBS -l mem=4GB
+#PBS -l pmem=4GB
+#PBS -l vmem=4GB
+#PBS -l nodes=1:ppn=1
+#PBS -l walltime=04:00:00
+#PBS -N SphereCup
+
+# Configure your environment
+export DIPY_DIR="$HOME/opt/dipy"
+export EXPERIMENTS_DIR="$HOME/opt/experiments"
+###################################
+echo "=====Dipy commit ====="
+(cd $DIPY_DIR && git branch|grep "*")
+(cd $DIPY_DIR && git show --stat|grep "commit\|Date")
+echo "====Experiments commit===="
+(cd $EXPERIMENTS_DIR && git branch|grep "*")
+(cd $EXPERIMENTS_DIR && git show --stat|grep "commit\|Date")
+echo "======================"
+date
+python sphere2cup.py
